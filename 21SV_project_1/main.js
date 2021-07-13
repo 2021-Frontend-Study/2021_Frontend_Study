9c6c0a9369972c0bb8194ab486791959faba2bf8
YimKeul <43426556+YimKeul@users.noreply.github.com>
2021-07-13 19:51:57 +0900

    ok top

--- 21SV_project_1/main.js
+++ 21SV_project_1/main.js
@@ -23,13 +23,12 @@ function blink() {
 }
 
 
-// const secIntro = document.querySelector(".secIntro");
-// const section1 = document.querySelector(".section1");
-// const section2 = document.querySelector(".section2");
-// const intro = document.querySelector(".Intro");
-// const Sec_1 = document.querySelector(".Sec_1");
-// const Sec_2 = document.querySelector(".Sec_2");
-
-// secIntro.addEventListener("click", () => window.scrollTo({ top: intro.offsetTop, behavior: 'smooth' }));
-// section1.addEventListener("click", () => window.scrollTo({ top: Sec_1.offsetTop, behavior: 'smooth' }));
-// section2.addEventListener("click", () => window.scrollTo({ top: Sec_2.offsetTop, behavior: 'smooth' }));
+const secIntro = document.querySelector(".secIntro");
+const section1 = document.querySelector(".section1");
+const section2 = document.querySelector(".section2");
+const intro = document.querySelector(".Intro");
+const Sec_1 = document.querySelector(".Sec_1");
+const Sec_2 = document.querySelector(".Sec_2");
+secIntro.addEventListener("click", () => window.scrollTo({ top: intro.offsetTop, behavior: 'smooth' }));
+section1.addEventListener("click", () => window.scrollTo({ top: Sec_1.offsetTop, behavior: 'smooth' }));
+section2.addEventListener("click", () => window.scrollTo({ top: Sec_2.offsetTop, behavior: 'smooth' }));
