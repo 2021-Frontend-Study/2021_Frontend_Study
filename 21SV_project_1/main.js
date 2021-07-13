809cbf5563105f7839eb9f36d8c4df9a7bd31626
choi hye rin <ypremium5959@gmail.com>
2021-07-13 15:08:23 +0900

    add typeranimationfirst by hyerin

---
+++ 21SV_project_1/main.js
@@ -0,0 +1,23 @@
+// let target = document.querySelector("#dynamic");
+// let stringArr =["s-v 여름프로젝트"];
+// function typer_String(){
+//     let selectString = stringArr[0];
+//     let selectStringArr = selectString.split("");
+    
+//     return selectStringArr;
+// }
+
+// function typer_text(stringArr){
+//     if(stringArr.length>0){
+//         target.textContent += stringArr.shift();
+//         setTimeout(function(){
+//             typer_text(stringArr);
+//         },300);
+//     }
+// }
+// typer_text(typer_String());
+
+
+// function blink() {
+//     target.classList.toggle("active");
+// }
