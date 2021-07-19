c2b7bfd6ca31cf380c9335d20e4d5469d803efcf
ShinChanU <tlscksdn963@naver.com>
2021-07-19 23:30:33 +0900

    aos test

---
+++ test/js/scroll.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// const project_listUl = document.querySelector(".project_list ul");
+// const chats = document.querySelectorAll(".chat");
+// const scrollY = window.scrollY;
+// const winH = window.innerHeight;
+
+// function checkPosition() {
+//   for (let i = 0; i < chats.length; i++) {
+//     const posFromTop = chats[i].getBoundingClientRect().top;
+//     if (winH > posFromTop && (posFromTop !== 0)) {
+//       chats[i].classList.remove("hidden");
+//     } else {
+//       chats[i].classList.add("hidden");
+//     }
+//   }
+// }
+
+// window.addEventListener("scroll", checkPosition);
+
+AOS.init();
