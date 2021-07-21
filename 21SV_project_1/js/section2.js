f7a3534788818cdb692ccded9e2a9866636416ce
ShinChanU <tlscksdn963@naver.com>
2021-07-21 18:36:41 +0900

    sec2 add clock

--- 21SV_project_1/js/section2.js
+++ 21SV_project_1/js/section2.js
@@ -58,8 +58,8 @@ const sec2 = document.querySelector(".sec_2");
 
 sec2.addEventListener("click", () => window.scrollTo({ top: sec2.offsetTop, behavior: 'smooth' }));
 
-// chatting clock
-function getClock() {
+// chatting date 
+function getDate() {
   const chatDate = document.querySelector(".chatDate");
   const kor_day = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
   const date = new Date();
@@ -68,6 +68,25 @@ function getClock() {
   const dates = date.getDate();
   const day = kor_day[date.getDay()];
   chatDate.innerText = `${year}년 ${month}월 ${dates}일 ${day}`;
+}
+
+getDate();
+
+// chatting clock
+function getClock() {
+  const chat__clock = document.querySelectorAll(".chat__clock");
+  for (let i = 0; i < chat__clock.length; i++) {
+    const element = chat__clock[i];
+    const date = new Date();
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    let hour_12 = "오전";
+    if (hours >= 12) {
+      hours = hours - 12;
+      hour_12 = "오후";
+    }
+    element.innerText = `${hour_12} ${hours}:${minutes}`;
+  }
 }
 
 getClock();
